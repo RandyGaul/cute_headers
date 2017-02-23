82c7d69052c147d54432e5ed6a4f763e42fbbf12
RandyGaul <r.gaul@digipen.edu>
2017-02-22 23:54:07 -0800

    license

--- tinyutf.h
+++ tinyutf.h
@@ -156,3 +156,13 @@ void tuShorten( const wchar_t* tu_utf16_text, int out_size, char* tu_out_char )
 }
 
 #endif
+
+/*
+	This is free and unencumbered software released into the public domain.
+	Our intent is that anyone is free to copy and use this software,
+	for any purpose, in any form, and by any means.
+	The authors dedicate any and all copyright interest in the software
+	to the public domain, at their own expense for the betterment of mankind.
+	The software is provided "as is", without any kind of warranty, including
+	any implied warranty. If it breaks, you get to keep both pieces.
+*/
