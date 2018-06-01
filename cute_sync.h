e45a877aa4c4659151ab09243586ed5bee935cd9
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2018-05-31 20:32:50 -0700

    license and doc tweaks

--- cute_sync.h
+++ cute_sync.h
@@ -191,7 +191,8 @@ typedef struct cute_rw_lock_t
 } cute_rw_lock_t;
 
 /**
- * Constructs an unlocked mutual exclusion read/write lock.
+ * Constructs an unlocked mutual exclusion read/write lock. The `rw` lock can safely sit
+ * on the stack.
  */
 void cute_rw_lock_create(cute_rw_lock_t* rw);
 
@@ -217,6 +218,10 @@ void cute_write_lock(cute_rw_lock_t* rw);
  * Undoes a single call to `cute_write_lock`.
  */
 void cute_write_unlock(cute_rw_lock_t* rw);
+
+/**
+ * Destroys the internal semaphores, and mutex.
+ */
 void cute_rw_lock_destroy(cute_rw_lock_t* rw);
 
 typedef struct cute_threadpool_t cute_threadpool_t;
@@ -692,3 +697,43 @@ void cute_threadpool_destroy(cute_threadpool_t* pool)
 
 #endif // CUTE_SYNC_IMPLEMENTATION_ONCE
 #endif // CUTE_SYNC_IMPLEMENTATION
+
+/*
+	------------------------------------------------------------------------------
+	This software is available under 2 licenses - you may choose the one you like.
+	------------------------------------------------------------------------------
+	ALTERNATIVE A - zlib license
+	Copyright (c) 2018 Randy Gaul http://www.randygaul.net
+	This software is provided 'as-is', without any express or implied warranty.
+	In no event will the authors be held liable for any damages arising from
+	the use of this software.
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+	  1. The origin of this software must not be misrepresented; you must not
+	     claim that you wrote the original software. If you use this software
+	     in a product, an acknowledgment in the product documentation would be
+	     appreciated but is not required.
+	  2. Altered source versions must be plainly marked as such, and must not
+	     be misrepresented as being the original software.
+	  3. This notice may not be removed or altered from any source distribution.
+	------------------------------------------------------------------------------
+	ALTERNATIVE B - Public Domain (www.unlicense.org)
+	This is free and unencumbered software released into the public domain.
+	Anyone is free to copy, modify, publish, use, compile, sell, or distribute this 
+	software, either in source code form or as a compiled binary, for any purpose, 
+	commercial or non-commercial, and by any means.
+	In jurisdictions that recognize copyright laws, the author or authors of this 
+	software dedicate any and all copyright interest in the software to the public 
+	domain. We make this dedication for the benefit of the public at large and to 
+	the detriment of our heirs and successors. We intend this dedication to be an 
+	overt act of relinquishment in perpetuity of all present and future rights to 
+	this software under copyright law.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+	AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+	ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	------------------------------------------------------------------------------
+*/
