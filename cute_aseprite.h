eb1d8024b1174cf89a95c78d24953462df1e49c5
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2020-08-25 01:19:18 -0700

    forgot license

--- cute_aseprite.h
+++ cute_aseprite.h
@@ -619,7 +619,7 @@ static int s_block(deflate_t* s)
 ase_err:
 	return 0;
 }
-	
+
 // 3.2.3
 static int s_inflate(const void* in, int in_bytes, void* out, int out_bytes, void* mem_ctx)
 {
@@ -640,7 +640,7 @@ static int s_inflate(const void* in, int in_bytes, void* out, int out_bytes, voi
 
 	s->final_word_available = last_bytes ? 1 : 0;
 	s->final_word = 0;
-	for(int i = 0; i < last_bytes; i++) 
+	for(int i = 0; i < last_bytes; i++)
 		s->final_word |= ((uint8_t*)in)[in_bytes - last_bytes+i] << (i * 8);
 
 	s->count = first_bytes * 8;
@@ -844,7 +844,7 @@ ase_t* cute_aseprite_load_from_memory(const void* memory, int size, void* mem_ct
 {
 	ase_t* ase = (ase_t*)CUTE_ASEPRITE_ALLOC(sizeof(ase_t), mem_ctx);
 	CUTE_ASEPRITE_MEMSET(ase, 0, sizeof(*ase));
-	
+
 	ase_state_t state = { 0 };
 	ase_state_t* s = &state;
 	s->in = (uint8_t*)memory;
@@ -1154,3 +1154,43 @@ void cute_aseprite_free(ase_t* ase)
 
 #endif // CUTE_ASEPRITE_IMPLEMENTATION_ONCE
 #endif // CUTE_ASEPRITE_IMPLEMENTATION
+
+/*
+	------------------------------------------------------------------------------
+	This software is available under 2 licenses - you may choose the one you like.
+	------------------------------------------------------------------------------
+	ALTERNATIVE A - zlib license
+	Copyright (c) 2017 Randy Gaul http://www.randygaul.net
+	This software is provided 'as-is', without any express or implied warranty.
+	In no event will the authors be held liable for any damages arising from
+	the use of this software.
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+	  1. The origin of this software must not be misrepresented; you must not
+	     claim that you wrote the original software. If you use this software
+	     in a product, an acknowledgment in the product documentation would be
+	     appreciated but is not required.
+	  2. Altered source versions must be plainly marked as such, and must not
+	     be misrepresented as being the original software.
+	  3. This notice may not be removed or altered from any source distribution.
+	------------------------------------------------------------------------------
+	ALTERNATIVE B - Public Domain (www.unlicense.org)
+	This is free and unencumbered software released into the public domain.
+	Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+	software, either in source code form or as a compiled binary, for any purpose,
+	commercial or non-commercial, and by any means.
+	In jurisdictions that recognize copyright laws, the author or authors of this
+	software dedicate any and all copyright interest in the software to the public
+	domain. We make this dedication for the benefit of the public at large and to
+	the detriment of our heirs and successors. We intend this dedication to be an
+	overt act of relinquishment in perpetuity of all present and future rights to
+	this software under copyright law.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+	ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	------------------------------------------------------------------------------
+*/
