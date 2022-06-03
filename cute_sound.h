a276fad10b62e2f56fa676a3388ce212582b6cd4
Konrad Kubacki <Condzi@users.noreply.github.com>
2022-06-03 17:04:19 +0200

    cute_sound: comment on arguments in cs_init

--- cute_sound.h
+++ cute_sound.h
@@ -223,6 +223,8 @@ const char* cs_error_as_string(cs_error_t error);
 /**
  * Pass in NULL for `os_handle`, except for the DirectSound backend this should
  * be hwnd.
+ * play_frequency_in_Hz depends on your audio file, 44100 seems to be fine.
+ * buffered_samples is clamped to be at least 1024.
  */
 cs_error_t cs_init(void* os_handle, unsigned play_frequency_in_Hz, int buffered_samples, void* user_allocator_context /* = NULL */);
 void cs_shutdown();
