ca994b81fb8fdb1a33a5b2116acc02d2b1ab8aec
RandyGaul <randypgaul@gmail.com>
2022-11-19 00:37:33 -0800

    V bump

--- cute_png.h
+++ cute_png.h
@@ -3,7 +3,7 @@
 		Licensing information can be found at the end of the file.
 	------------------------------------------------------------------------------
 
-	cute_png.h - v1.04
+	cute_png.h - v1.05
 
 	To create implementation (the function definitions)
 		#define CUTE_PNG_IMPLEMENTATION
