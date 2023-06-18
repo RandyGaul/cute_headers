31b492b8db9d75423787ff0db760a5d42f54b7ec
RandyGaul <randypgaul@gmail.com>
2023-06-17 23:57:28 -0700

    Quick windows fix + license

--- cute_tls.h
+++ cute_tls.h
@@ -663,7 +663,7 @@ TLS_State tls_process(TLS_Connection connection)
 					}
 				}
 				if (ctx->tcp_connect_pending) {
-					return;
+					return ctx->state;
 				}
 			}
 	
@@ -725,11 +725,11 @@ TLS_State tls_process(TLS_Connection connection)
 					// Successfully completed handshake. TLS tunnel is now operational.
 					QueryContextAttributes(&ctx->context, SECPKG_ATTR_STREAM_SIZES, &ctx->sizes);
 					ctx->state = TLS_STATE_CONNECTED;
-					return;
+					return ctx->state;
 				} else if (sec == SEC_I_INCOMPLETE_CREDENTIALS) {
 					// Client certs are not supported.
 					ctx->state = TLS_STATE_SERVER_ASKED_FOR_CLIENT_CERTS;
-					return;
+					return ctx->state;
 				} else if (sec == SEC_I_CONTINUE_NEEDED) {
 					// Continue sending data to the server.
 					char* buffer = (char*)outbuffers[0].pvBuffer;
@@ -746,7 +746,7 @@ TLS_State tls_process(TLS_Connection connection)
 					if (size != 0) {
 						// Somehow failed to send() data to server.
 						ctx->state = TLS_STATE_UNKNOWN_ERROR;
-						return;
+						return ctx->state;
 					}
 				} else if (sec != SEC_E_INCOMPLETE_MESSAGE) {
 					if (sec == SEC_E_CERT_EXPIRED) {
@@ -760,7 +760,7 @@ TLS_State tls_process(TLS_Connection connection)
 					} else {
 						ctx->state = TLS_STATE_UNKNOWN_ERROR;
 					}
-					return;
+					return ctx->state;
 				} else {
 					TLS_ASSERT(sec == SEC_E_INCOMPLETE_MESSAGE);
 					// Need to read more bytes.
@@ -770,7 +770,7 @@ TLS_State tls_process(TLS_Connection connection)
 			if (ctx->received == sizeof(ctx->incoming)) {
 				// Server is sending too much data instead of proper handshake?
 				ctx->state = TLS_STATE_UNKNOWN_ERROR;
-				return;
+				return ctx->state;
 			}
 	
 			// 4. Read data from the server (recv).
@@ -814,14 +814,14 @@ TLS_State tls_process(TLS_Connection connection)
 				} else if (sec == SEC_I_CONTEXT_EXPIRED) {
 					// Server closed TLS connection (but socket is still open).
 					ctx->state = TLS_STATE_DISCONNECTED;
-					return;
+					return ctx->state;
 				} else if (sec == SEC_I_RENEGOTIATE) {
 					// Server wants to renegotiate TLS connection, not implemented here.
 					ctx->state = TLS_STATE_UNKNOWN_ERROR;
-					return;
+					return ctx->state;
 				} else if (sec != SEC_E_INCOMPLETE_MESSAGE) {
 					ctx->state = TLS_STATE_UNKNOWN_ERROR;
-					return;
+					return ctx->state;
 				} else {
 					TLS_ASSERT(sec == SEC_E_INCOMPLETE_MESSAGE);
 					// More data needs to be read.
@@ -1047,3 +1047,43 @@ int tls_send(TLS_Connection connection, const void* data, int size)
 
 #endif // CUTE_TLS_IMPLEMENTATION_ONCE
 #endif // CUTE_TLS_IMPLEMENTATION
+
+/*
+	------------------------------------------------------------------------------
+	This software is available under 2 licenses - you may choose the one you like.
+	------------------------------------------------------------------------------
+	ALTERNATIVE A - zlib license
+	Copyright (c) 2023 Randy Gaul https://randygaul.github.io/
+	This software is provided 'as-is', without any express or implied warranty.
+	In no event will the authors be held liable for any damages arising from
+	the use of this software.
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+	  1. The origin of this software must not be misrepresented; you must not
+	     claim that you wrote the original software. If you use this software
+	     in a product, an acknowledgment in the product documentation would be
+	     appreciated but is not required.
+	  2. Altered source versions must be plainly marked as such, and must not
+	     be misrepresented as being the original software.
+	  3. This notice may not be removed or altered from any source distribution.
+	------------------------------------------------------------------------------
+	ALTERNATIVE B - Public Domain (www.unlicense.org)
+	This is free and unencumbered software released into the public domain.
+	Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+	software, either in source code form or as a compiled binary, for any purpose,
+	commercial or non-commercial, and by any means.
+	In jurisdictions that recognize copyright laws, the author or authors of this
+	software dedicate any and all copyright interest in the software to the public
+	domain. We make this dedication for the benefit of the public at large and to
+	the detriment of our heirs and successors. We intend this dedication to be an
+	overt act of relinquishment in perpetuity of all present and future rights to
+	this software under copyright law.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+	ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	------------------------------------------------------------------------------
+*/
