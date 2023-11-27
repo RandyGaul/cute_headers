a6dff50ab93e5a7a0307eee77f9ac2be795a138c
Guilhermo Barretti <guilhermobarretti@gmail.com>
2023-11-27 10:51:34 -0300

    Added support for ase_tag_t repeat field

--- cute_aseprite.h
+++ cute_aseprite.h
@@ -213,6 +213,7 @@ struct ase_tag_t
 	int from_frame;
 	int to_frame;
 	ase_animation_direction_t loop_animation_direction;
+	int repeat;
 	uint8_t r, g, b;
 	const char* name;
 	ase_udata_t udata;
@@ -1100,7 +1101,8 @@ ase_t* cute_aseprite_load_from_memory(const void* memory, int size, void* mem_ct
 					tag.from_frame = (int)s_read_uint16(s);
 					tag.to_frame = (int)s_read_uint16(s);
 					tag.loop_animation_direction = (ase_animation_direction_t)s_read_uint8(s);
-					s_skip(s, 8); // For future (set to zero).
+					tag.repeat = s_read_uint16(s);
+					s_skip(s, 6); // For future (set to zero).
 					tag.r = s_read_uint8(s);
 					tag.g = s_read_uint8(s);
 					tag.b = s_read_uint8(s);
